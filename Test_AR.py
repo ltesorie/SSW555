f90e955ebf835b17dff725bba7e8426ed05c0fbe
Alyson Randall <you@example.com>
2019-02-24 18:29:16 -0500

    modifying alyson's user story tests

--- Test_AR.py
+++ Test_AR.py
@@ -4,6 +4,7 @@
 import unittest
 from Functions import US03, US04
 
+
 class test(unittest.TestCase):
     def test_US03(self):
         date1 = "24 FEB 2019"
@@ -11,15 +12,10 @@ class test(unittest.TestCase):
         date3 = "9 NOV 2011"
         date4 = "6 OCT 1995"
 
-
         self.assertEqual(US03(date1, date2), True)
-
         self.assertEqual(US03(date2, date1), False)
-
         self.assertEqual(US03(date3, date4), False)
-
         self.assertEqual(US03(date4, date3), True)
-
         self.assertEqual(US03(date4, date1), True)
 
     def test_US04(self):
@@ -29,14 +25,10 @@ class test(unittest.TestCase):
         date4 = "6 OCT 1995"
 
         self.assertEqual(US04(date1, date2), True)
-
         self.assertEqual(US04(date2, date1), False)
-
         self.assertEqual(US04(date3, date4), False)
-
         self.assertEqual(US04(date4, date3), True)
+        self.assertEqual(US04(date1, date1), False)
 
-        self.assertEqual(US04(date4, date1), True)
-
-
-
+if __name__ == '__main__':
+    unittest.main()
