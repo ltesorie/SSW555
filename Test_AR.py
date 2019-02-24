784b2f10f096ef17847e456abf35a604b9c5565d
Alyson Randall <you@example.com>
2019-02-24 17:58:52 -0500

    Alyson's user story tests

---
+++ Test_AR.py
@@ -0,0 +1,42 @@
+# Author: Alyson Randall
+# User Story Tests
+
+import unittest
+from Functions import US03, US04
+
+class test(unittest.TestCase):
+    def test_US03(self):
+        date1 = "24 FEB 2019"
+        date2 = "13 MAY 2032"
+        date3 = "9 NOV 2011"
+        date4 = "6 OCT 1995"
+
+
+        self.assertEqual(US03(date1, date2), True)
+
+        self.assertEqual(US03(date2, date1), False)
+
+        self.assertEqual(US03(date3, date4), False)
+
+        self.assertEqual(US03(date4, date3), True)
+
+        self.assertEqual(US03(date4, date1), True)
+
+    def test_US04(self):
+        date1 = "24 FEB 2019"
+        date2 = "13 MAY 2032"
+        date3 = "9 NOV 2011"
+        date4 = "6 OCT 1995"
+
+        self.assertEqual(US04(date1, date2), True)
+
+        self.assertEqual(US04(date2, date1), False)
+
+        self.assertEqual(US04(date3, date4), False)
+
+        self.assertEqual(US04(date4, date3), True)
+
+        self.assertEqual(US04(date4, date1), True)
+
+
+
